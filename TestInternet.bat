5e24248afa53c01e9380124ad4e5d85d187dea99
Anand Deshbhratar <anandd3@gmail.com>
2019-03-30 13:20:26 +0530

    Internet connection test.
    This file will check internet connection in every 5 seconds. And dump the date and time when internet in not available.
    
    NOTE: Before running the file on open cmd as "Run as Administrator."

---
+++ TestInternet.bat
@@ -0,0 +1,7 @@
+@echo off
+SET "count = 1"
+:while
+ping google.com > null && (echo %date% %time% You are connected to the internet! > null) || (echo %date% %time% You are not connected to the internet! >> not_connected.log)
+timeout /t 5 /nobreak > null
+goto :while
+
